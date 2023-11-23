1a84d23affae83d86b76765bf376db99cdd4f62d
João Marcos <47647644+JoaoMarcosSC@users.noreply.github.com>
2023-11-22 23:31:25 -0300

    Adições

---
+++ Curso-PHP/recursos/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap">
+    <link rel="stylesheet" href="../../assets/css/estilo.css">
+
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Indice Exercicios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Modulo 01</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Modulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo azul">
+                    <h3>Modulo 03</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo roxo">
+                    <h3>Modulo 04</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo amarelo">
+                    <h3>Modulo 05</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo verde-escuro">
+                    <h3>Modulo 06</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho-escuro">
+                    <h3>Modulo 07</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo azul-escuro">
+                    <h3>Modulo 08</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo roxo-escuro">
+                    <h3>Modulo 09</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        
+    </main>
+    <footer class="rodape">
+        João Marcos © 2023
+    </footer>
+</body>
+
+</html>
